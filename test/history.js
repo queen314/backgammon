76885cf9d9509b11f4563bb665948fc400891e9b
Yern Sun <yernsun@yernsun.com>
2017-07-30 17:51:10 +0800

    add history test

--- test/history.js
+++ test/history.js
@@ -7,17 +7,41 @@
 
 const assert = require('assert');
 
-const ChessGame = require('../src/js/utils/history');
+const History = require('../src/js/utils/history');
 
 
 describe('History', function () {
-    const board = new Board();
-    const position = { x: 1, y: 1 };
-    const positionValue = 2;
+
     // board
     describe('#push', function () {
-        it(`should be 0 if the position of ${JSON.stringify(position)} is not put piece`, function () {
-            assert.equal(0, board.get(position));
+        it(`push to the stack`, function () {
+            const history = new History();
+            const item = [1, 2, 3];
+            history.push(item);
+            assert.equal(item, history.last);
+            // assert.equal(0, board.get(position));
+        });
+    });
+
+    describe('#undo', function () {
+        it(`undo `, function () {
+            const history = new History();
+            const item = [1, 2, 3];
+            history.push(item);
+            let undo = history.undo();
+            assert.equal(item, undo);
+        });
+    });
+
+
+    describe('#redo', function () {
+        it(`undo `, function () {
+            const history = new History();
+            const item = [1, 2, 3];
+            history.push(item);
+            history.undo();
+            let undo = history.redo();
+            assert.equal(item, undo);
         });
     });
 });
